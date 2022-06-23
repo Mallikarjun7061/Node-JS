8a037f7217b59c96d247e278261aae9c02839ee7
Malliakrjun <Mallikarjunwarda8@gmail.com>
2022-06-23 11:23:11 +0530

    count

--- stuff.js
+++ stuff.js
@@ -8,6 +8,12 @@ return `The sum of the 2 numbers is ${a+b}`;
  
 var pi=3.142;
 
-module.exports.counter = counter;
-module.exports.adder = adder;
-module.exports.pi = pi;
+// module.exports.counter = counter;
+// module.exports.adder = adder;
+// module.exports.pi = pi;
+
+module.exports={
+    counter:counter,
+    adder : adder,
+    pi : pi
+};
