f0ba711992e0347f2c04c2e661f572a28136d062
Malliakrjun <Mallikarjunwarda8@gmail.com>
2022-06-25 09:27:02 +0530

    HTML

---
+++ Serving_html.js
@@ -0,0 +1,12 @@
+var http = require('http');
+var fs =require ('fs');
+
+
+var server = http.createServer(function(req,res){
+console.log('request was made :' +req.ulr);
+res.writeHead(200,{'content-type':'text/html'});
+var myReadStreams=fs.createReadStream(__dirname + '/Index.html','utf8');
+myReadStreams.pipe(res);
+});
+ server.listen(3000,'127.0.0.1');
+ console.log('listening to port 3000 ');
