57152132d9a81fa8bba89491eaf960b7b58a121d
Malliakrjun <Mallikarjunwarda8@gmail.com>
2022-06-27 09:18:25 +0530

    App

---
+++ public/controllers/todoController.js
@@ -0,0 +1,18 @@
+module.exports =function(app){
+
+    app.get('/todo',function(req,res){
+    res.render('todo');
+    });
+
+
+    app.get('/todo',function(req,res){
+
+    });
+
+
+    app.get('/todo',function(req,res){
+
+    });
+
+
+};
